f007cf19d39f981768e81a75ccedcf1bd4b75dbe
Juanita Munar <juanita.munar0.0@gmail.com>
2024-04-17 00:26:57 -0500

    v0.0.1

--- programa.py
+++ programa.py
@@ -1,1 +1,10 @@
+#Importar Numpy
 import numpy as np
+#Crear una matriz cuadrada "a" de dos dimensiones
+a=np.array([[6,9],[3,7]])
+#usando la funcion linalg.eig buscamos los valores y vectores propios
+#Y se los asignamos a vectores_p y valores_p
+vectores_p, valores_p =np.linalg.eig(a)
+print(a)
+print(vectores_p)
+print(valores_p)
